f77a9280102026ccbeb2ceadebb49895934d1633
Enrique Marina <eherraiz@myuax.com>
2022-03-27 19:14:43 +0200

    hecho

--- a)El_dia_siguiente.py
+++ a)El_dia_siguiente.py
@@ -1,27 +1,74 @@
-class Empleado:
-    def __del__(self):
-        print(f'El empleado {self.name} a sido despedido de la empresa devido a la desapariciónde la misma')
-    def __init__(self, name):
-        self.name = name
-
 class Edificio:
-    def __del__(self):
-        print(f'El edificio {self.name} es destruido')
     def __init__(self, name):
         self.name = name
         self.ciudad = None
+        self.empresa = None
+
+    def obtener_edificio(self):
+        return self.name
+
+    def __del__(self):
+        print(f'El edificio {self.name} es destruido')
+
+class Empleado:
+    def __init__(self, name):
+        self.name = name
+        self.empresa = None
+    def __del__(self):
+        print(f'El empleado {self.name} es despedido, la empresa quebró por la falta de instalaciones')
+
 
 class Ciudad:
-    def __del__(self):
-        print(f'{self.name} fue desturida devido a la catastrofe')
-    def __init__(self, edificios_ciudad):
-        self.edificios = [Edificio(name) for name in edificios_ciudad]
 
-class Empresa:
-    def __del__(self):
-        print(f'La empresa {self.name} a dejado de operar devido a que ya no le queda ninguna sede')
-    def __init__(self, edificios_empresa, empleados):
-        self.edificios = [Edificio(name) for name in edificios_empresa]
-        self.trabajador = [Empleado(name) for name in empleados]
+    def __init__(self, nombre):
+        self.edificios = []
+        self.nombre = nombre
 
-empresaA = Empresa(['A', 'B', 'C'], ['Martin', 'Salim', 'Xing'])
+    def anadir_edificios(self, lista):
+        for i in range(len(lista)):
+            self.edificios.append(Edificio(lista[i]))
+
+    def obtener_edificios(self):
+        return self.edificios
+
+    def __del__(self):
+        print(f'La ciudad {self.nombre} fue destruida por una catastrofe')
+
+class Empresa():
+
+    def __init__(self, nombre_empresa):
+        self.nombre_empresa = nombre_empresa
+        self.empleados = []
+        self.edificios = []
+
+    def añadir_empleados(self, lista):
+        for i in range(len(lista)):
+            self.empleados.append(Empleado(lista[i]))
+
+    def añadir_edificios(self, lista):
+        for i in range(len(lista)):
+            x = lista[i].obtener_edificio()
+            opcion = input(f'¿Es el edificio {x} parte de {self.nombre_empresa}?\n')
+            if opcion.lower == 'SI':
+                self.edificios.append(lista[i])
+
+    def obtener_edificios(self):
+        return self.edificios
+
+    def __del__(self):
+        print(f'La empresa {self.nombre_empresa} quiebra por falta de instalaciones')
+
+nueva_york = Ciudad('Nueva York')
+LA = Ciudad('Los Angeles')
+nueva_york.anadir_edificios(['A', 'B'])
+LA.anadir_edificios(['C'])
+print(nueva_york.obtener_edificios())
+YooHoo = Empresa('YooHoo')
+YooHoo.añadir_empleados(['Martin', 'Salim', 'Xing'])
+YooHoo.añadir_edificios(nueva_york.obtener_edificios())
+YooHoo.añadir_edificios(LA.obtener_edificios())
+del nueva_york
+
+
+
+
